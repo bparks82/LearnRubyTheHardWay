a63387f747e485dbb179396ba6eaeb1aaff37530
Brian Parks <bpparks@gmail.com>
2012-08-28 10:13:55 -0600

    exercise 1 completed with extra credit

---
+++ ex1.rb
@@ -0,0 +1,35 @@
+=begin
+#this is the original exercise
+puts "Hello World!"
+puts "Hello Again"
+puts "I like typing this."
+puts "This is fun."
+puts 'Yay! Printing.'
+puts "I'd much rather you 'not'."
+puts 'I "said" do not touch this.'
+#=end
+
+
+=begin
+#this includes the extra credit with another printed line
+puts "Hello World!"
+puts "Hello Again"
+puts "I like typing this."
+puts "This is fun."
+puts 'Yay! Printing.'
+puts "I'd much rather you 'not'."
+puts 'I "said" do not touch this.'
+puts "Making script print another line"
+=end
+
+#=begin
+#this includes the extra credit with only one of the lines being printed
+puts "Hello World!"
+"Hello Again"
+"I like typing this."
+"This is fun."
+'Yay! Printing.'
+"I'd much rather you 'not'."
+'I "said" do not touch this.'
+"Making script print another line"
+#=end
